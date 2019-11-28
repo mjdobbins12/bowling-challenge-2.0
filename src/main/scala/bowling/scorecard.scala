20dee3d6277299c71551371ce3c5f82cec9b110e
Mark Dobbins <mjdobbins12@gmail.com>
2019-11-28 16:01:53 +0000

    bonus for one strike

--- src/main/scala/bowling/scorecard.scala
+++ src/main/scala/bowling/scorecard.scala
@@ -2,8 +2,12 @@ package bowling
 
 class Scorecard {
 	var score = 0
+	var bonus = 1
 	
-	def recordRoll(roll: Int) {
-		this.score += roll
+	def recordRoll(pins: Int) {
+		this.score += (pins * this.bonus)
+		if (pins == 10) {
+			bonus += 1
+		}
 	}
 }
